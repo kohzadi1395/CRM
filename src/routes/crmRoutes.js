4f78289635b0dc2f68d3a346c47cea15b04e7125
kohzadi <kohzadi_hossein@yahoo.com>
2019-01-09 16:24:18 +0330

    middleware

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,14 +1,21 @@
 const routes = (app) => {
     app.route('/contact')
-        .get((req, res) =>
-            res.send('Get request successfull!!'))
+        .get((req, res, next) => {
+                //middleware
+                console.log(`Request from :${req.originalUrl}`);
+                console.log(`Request Type :${req.method}`);
+                next();
+            }, (req, res, next) => {
+                res.send('Get request successful!!');
+            }
+        )
         .post((req, res) =>
-            res.send('Post request successfull!!'));
+            res.send('Post request successful!!'));
 
     app.route('/contact/:contactId')
         .put((req, res) =>
-            res.send('Put request successfull!!'))
+            res.send('Put request successful!!'))
         .delete((req, res) =>
-            res.send('Delete request successfull!!'));
+            res.send('Delete request successful!!'));
 };
 export default routes;
